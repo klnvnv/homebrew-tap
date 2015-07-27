cdaa8641de7ac8f713f49f1e148073401affa624
kln <klnvnv@gmail.com>
2015-07-27 17:19:42 +0300

    emacs-latest-git trying to understand the formulas

--- emacs-latest-git.rb
+++ emacs-latest-git.rb
@@ -3,12 +3,14 @@ class Emacs < Formula
   homepage "https://www.gnu.org/software/emacs/"
   url "https://github.com/emacs-mirror/emacs.git"
 
+  version "0.1"
+
   depends_on "imagemagick"
 
   def install
     system "./autogen.sh"
     system "CPPFLAGS=`xml2-config --cflags` ./configure" "--with-ns"
     system "CPPFLAGS=`xml2-config --cflags` make"
-    system "CPPFLAGS=`xml2-config --cflags` make -j3 install"
+    system "CPPFLAGS=`xml2-config --cflags` make" "install"
   end
 end
