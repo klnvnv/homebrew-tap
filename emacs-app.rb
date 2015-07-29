eb61d9106014df9967fa523b92981d276035aec3
kln <klnvnv@gmail.com>
2015-07-29 16:31:06 +0300

    seems to be doing what I want it to, somewhat.

--- emacs-app.rb
+++ emacs-app.rb
@@ -5,8 +5,6 @@ class EmacsApp < Formula
   homepage "https://www.gnu.org/software/emacs/"
   url "https://github.com/emacs-mirror/emacs.git"
 
-  keg_only "Have to move the app out of the Cellar anyway"
-  
   version Time.now.strftime("%d-%m-%Y-%H-%M")
 
   depends_on "autoconf" => :build
@@ -20,7 +18,14 @@ class EmacsApp < Formula
     system "./autogen.sh"
     system "./configure --with-ns"
     system "make -j#{cpus}"
-    system "make -j{cpus} install"
+    system "make -j#{cpus} install"
     prefix.install "nextstep/Emacs.app"
+  end
+
+  def caveats; <<-EOS.undent
+    This formula packages emacs as an app, don't know how to handle that within
+    a formula yet tho.
+    Too much code to read through, docs are shit.
+    EOS
   end
 end
---
+++ readme.md
@@ -0,0 +1,2 @@
+Compiles and packages the latest git version of emacs from the github mirror.
+Links the app from the Cellar to /Applications
